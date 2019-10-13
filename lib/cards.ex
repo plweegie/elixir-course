e01be40f0fb998b4513ae75310e5cdee28ab3c15
Jszymanski <jan.kazimierz.szymanski@gmail.com>
2019-10-13 21:32:29 +0100

    Add docs and tests

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,4 +1,11 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
+  @doc """
+    Returns a list of strings representing a deck of playing cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
@@ -12,10 +19,33 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines whether a deck contains a given card.
+
+  ## Examples
+
+    iex> deck = Cards.create_deck
+    iex> Cards.contains?(deck, "Two of Hearts")
+    true
+    iex> Cards.contains?(deck, "Eleven of Hearts")
+    false
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should
+    be in the hand.
+
+  ## Examples
+
+    iex> deck = Cards.create_deck
+    iex> {hand, deck} = Cards.deal(deck, 1)
+    iex> hand
+    ["Ace of Spades"]
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
@@ -26,13 +56,23 @@ defmodule Cards do
   end
 
   def load(filename) do
-
     case File.read(filename) do
       {:ok, binary} -> :erlang.binary_to_term binary
       {:error, _reason} -> "That file does not exist"
     end
   end
 
+  @doc """
+    Create a hand of a size equal to `hand_size`. Returns
+    a tuple with the hand as the first element and the
+    remainder of the deck as the second.
+
+  ## Examples
+
+    iex> hand = Cards.create_hand(5)
+    iex> tuple_size(hand)
+    2
+  """
   def create_hand(hand_size) do
     Cards.create_deck
     |> Cards.shuffle
--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,19 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "create_deck makes 20 cards" do
+    list_length = length(Cards.create_deck)
+    assert list_length == 20
+  end
+
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck
+    assert deck != Cards.shuffle(deck)
+  end
+
+  test "creating a hand returns a hand of correct size" do
+    hand = Cards.create_hand(5)
+    dealt_hand = elem(hand, 0)
+    assert length(dealt_hand) == 5
   end
 end
